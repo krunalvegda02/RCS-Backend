9fa53d7f7d7b688afb9f3e4e64d5b336f0e8fd92
Krunal Vegda <Krunal.vegda02@gmail.com>
2025-12-31 22:07:01 +0530

    webhook light

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -10,7 +10,25 @@ module.exports = {
         PORT: 3000,
         REDIS_HOST: '127.0.0.1',
         REDIS_PORT: 6379,
-        MONGODB_URI: 'mongodb+srv://krunalvegda02:krunalvegda02@cluster0.jwybog2.mongodb.net/rcs?retryWrites=true&w=majority'
+        MONGODB_URI: 'mongodb+srv://krunalvegda02:krunalvegda02@cluster0.jwybog2.mongodb.net/rcs?retryWrites=true&w=majority',
+        // Cloudinary
+        CLOUDINARY_CLOUD_NAME: 'krunalvegda02',
+        CLOUDINARY_API_KEY: '528136484569676',
+        CLOUDINARY_API_SECRET: 'n2mD1KDBgelVDRstn2ZlD8F6jrY',
+        // JWT
+        JWT_SECRET: 'Z9a8xM!2kP@7Lq1dFhR3CwdwdwdWnA5SeTQ',
+        JWT_REFRESH_SECRET: 'Qp7FJc2Z01W5R!D6xB8dwdwLHTeKaM',
+        JWT_EXPIRE: '1d',
+        JWT_REFRESH_EXPIRE: '7d',
+        // CORS
+        CORS_ORIGIN: 'https://www.rcssender.com/',
+        FRONTEND_URL: 'https://rcssender.com/',
+        // Jio RCS
+        JIO_ENV: 'production',
+        JIOAPI_BASE_URL: 'https://api.businessmessaging.jio.com',
+        JIO_SECRET_KEY: 'f6a4d066-d976-4143-b8e4-cebf24d981b6',
+        JIO_SECRET_ID: '6927fac2a34077bfc4bd45a2',
+        JIO_ASSISTANT_ID: '6927fac2a34077bfc4bd45a2'
       },
       wait_ready: true,
       listen_timeout: 10000
