7fd0fc422f11e5315572826cc0fb84274cd9e474
Krunal Vegda <Krunal.vegda02@gmail.com>
2026-01-06 14:50:16 +0530

    .env send

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import dotenv from "dotenv";
 import { createServer } from "http";
 import { createServer as createNetServer } from "net";
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 import connectDB from "./db/index.js";
 import app from "./app.js";
 import JioRCSService from "./services/JioRCS.service.js";
 import { scheduleArchivedCampaignCleanup } from "./schedulers/campaign.scheduler.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 dotenv.config({
-  path: "./.env",
+  path: join(__dirname, '../.env'),
 });
 
 const port = process.env.PORT || 8000;
