af6119e546560ddbb78b1c255969114fe52d67e3
Krunal Vegda <Krunal.vegda02@gmail.com>
2026-02-16 11:13:39 +0530

    instances incressed

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -54,7 +54,7 @@ module.exports = {
     {
       name: 'webhook-consumer',
       script: 'src/workers/webhookConsumer.js',
-      instances: 8,
+      instances: 5,
       exec_mode: 'fork',
       env: {
         NODE_ENV: 'production',
@@ -78,7 +78,7 @@ module.exports = {
     {
       name: 'stats-consumer',
       script: 'src/workers/statsConsumer.js',
-      instances: 10,
+      instances: 15,
       exec_mode: 'fork',
       env: {
         NODE_ENV: 'production',
@@ -102,7 +102,7 @@ module.exports = {
     {
       name: 'batch-consumer',
       script: 'src/workers/batchEntriesConsumer.js',
-      instances: 3,
+      instances: 5,
       exec_mode: 'fork',
       env: {
         NODE_ENV: 'production',
